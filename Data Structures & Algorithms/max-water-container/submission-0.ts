74e1104d71fc8de476e66ee84723e4365830820d
Ivan <tan.ivancjq@gmail.com>
2026-05-30 18:39:27 +0800

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights: number[]): number {
+        let left = 0
+        let right = heights.length - 1
+        let minHeight = 0
+        let area = 0
+
+        while (right > left)
+        {
+            minHeight = Math.min(heights[left], heights[right])
+
+            if (heights[left] > heights[right])
+                right--
+            else
+                left++
+            
+            area = Math.max(area, minHeight * (right - left + 1))
+        }
+
+        return area
+    }
+}
