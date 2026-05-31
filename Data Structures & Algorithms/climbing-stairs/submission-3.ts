105030e1e4ee8bbc9df3cb542691a6a6e69b258e
Ivan <tan.ivancjq@gmail.com>
2026-06-01 00:54:37 +0800

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+   climbStairs(n: number): number {
+    if (n <= 2) return n;
+
+    let a = 1; // ways to reach step 1
+    let b = 2; // ways to reach step 2
+
+    for (let i = 3; i <= n; i++) {
+        const c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return b;
+}
+}
