089f85e88f7e72695c9198564443126fb02f14eb
Ivan <tan.ivancjq@gmail.com>
2026-06-09 22:32:19 +0800

    Add: longest-palindromic-substring - submission-1

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-1.ts
@@ -0,0 +1,48 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {string}
+     */
+    // T: O(n^2)
+    // S: O(n^2)
+
+    // Two pointers and recurisive algorithem
+    longestPalindrome(s: string): string {
+        const n = s.length 
+
+        if (n < 2) return s;
+
+        const dp: boolean[][] = Array.from( {length: n }, () => Array(n).fill(false))
+
+        let start = 0
+        let maxLength = 1
+
+        for (let i = 0 ; i < n ; i++)
+        {
+            dp[i][i] = true
+        }
+
+        for (let len = 2; len <= n; len++)
+        {
+            for (let left = 0; left <= n - len; left++)
+            {
+                const right = left + len - 1
+
+                if (s[left] === s[right]){
+                    if (len === 2 || dp[left + 1][right - 1])
+                    {
+                        dp[left][right] = true
+
+                        if (len > maxLength)
+                        {
+                            start = left
+                            maxLength = len
+                        }
+                    }
+                }
+            }
+        }
+        return s.substring(start, start + maxLength)
+
+    }
+}
