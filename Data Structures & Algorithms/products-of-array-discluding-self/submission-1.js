40a7698ef0d725997ecf16d036709f9300eb13ac
Ivan <tan.ivancjq@gmail.com>
2026-06-12 21:40:34 +0800

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    // T: O(n^2)
+    // S: O(n)
+    productExceptSelf(nums) {
+        let res = []
+
+        for (let i = 0; i < nums.length; i++)
+        {
+            let product = 1
+            for (let j = 0; j < nums.length; j++)
+            {
+                if (i !== j)
+                    product *= nums[j]
+            }
+
+            res[i] = product
+        }
+
+        return res
+    }
+}
