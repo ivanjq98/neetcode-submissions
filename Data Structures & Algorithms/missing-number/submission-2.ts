080ddd5f38fa8a0ea2c85911a8079bf6672e4458
Ivan <tan.ivancjq@gmail.com>
2026-06-12 15:42:12 +0800

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.ts
@@ -0,0 +1,9 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    missingNumber(nums: number[]): number {
+        return ((nums.length) * (nums.length + 1) / 2 - nums.reduce((acc, curr) => acc + curr, 0))
+    }
+}
