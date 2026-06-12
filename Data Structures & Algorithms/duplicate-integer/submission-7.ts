c49ced7c4ce13503e71aee32ff79f0ecaf911f9d
Ivan <tan.ivancjq@gmail.com>
2026-06-12 23:21:47 +0800

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.ts
@@ -0,0 +1,11 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums: number[]): boolean {
+        let unique = new Set(nums)
+
+        return unique.size != nums.length
+    }
+}
