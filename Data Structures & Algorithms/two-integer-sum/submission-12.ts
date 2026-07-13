bc52575d6abf9a9b303743f108b64f1fbbbbcc2e
Ivan <tan.ivancjq@gmail.com>
2026-07-13 14:46:12 +0800

    Add: two-integer-sum - submission-12

---
+++ Data Structures & Algorithms/two-integer-sum/submission-12.ts
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    // T: O(n)
+    // S: O{n}
+    twoSum(nums: number[], target: number): number[] {
+        let dic = new Map<number, number>()
+
+        for (let i = 0; i < nums.length; i++)
+        {
+            let diff = target - nums[i]
+            if (dic.has(diff))
+                return [dic.get(diff), i]
+            dic.set(nums[i], i)
+        }
+
+        return []
+    }
+}
